7324a94f6cbc634d0aa100bf8e951366126a1ead
wangfeishsh <baochunyu1987@163.com>
2018-01-17 11:55:39 +0800

    init

--- src/main/java/com/bao/msmetrics/MsMetricsApplication.java
+++ src/main/java/com/bao/msmetrics/MsMetricsApplication.java
@@ -1,18 +1,21 @@
 package com.bao.msmetrics;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.autoconfigure.ExportMetricWriter;
-import org.springframework.boot.actuate.metrics.statsd.StatsdMetricWriter;
+import org.springframework.boot.actuate.metrics.export.MetricExportProperties;
+import org.springframework.boot.actuate.metrics.repository.redis.RedisMetricRepository;
 import org.springframework.boot.actuate.metrics.writer.MetricWriter;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 
 @SpringBootApplication
 public class MsMetricsApplication {
 
-	@Value("${spring.application.name:application}.${random.value:0000}")
-	private String prefix = "metrics";
+    @Value("${spring.application.name:application}.${random.value:0000}")
+    private String prefix = "metrics";
 
 //	@Bean
 //	@ExportMetricWriter
@@ -20,7 +23,17 @@ public class MsMetricsApplication {
 //		return new StatsdMetricWriter(prefix, "localhost", 8125);
 //	}
 
-	public static void main(String[] args) {
-		SpringApplication.run(MsMetricsApplication.class, args);
-	}
+    @Autowired
+    RedisConnectionFactory connectionFactory;
+
+    @Bean
+    @ExportMetricWriter
+    MetricWriter metricWriter(MetricExportProperties export) {
+        return new RedisMetricRepository(connectionFactory,
+                export.getRedis().getPrefix(), export.getRedis().getKey());
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsMetricsApplication.class, args);
+    }
 }
